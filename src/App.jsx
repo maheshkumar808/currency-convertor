bee79a87adf20a3ca044f7d83649bfee230e70b7
maheshkumar808 <808maheshkumar@gmail.com>
2024-11-14 10:42:23 +0530

    base setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [first, setfirst] = useState(second);
+
+  const abc = () => {
+    console.log('abcccccc with format');
+  };
+  return (
+    <div className="bg-green-800 h-9">
+      <div>
+        <h3>dscdsvs</h3>
+      </div>
+      <button>dsdscs </button>
+    </div>
+  );
+}
+
+export default App;
